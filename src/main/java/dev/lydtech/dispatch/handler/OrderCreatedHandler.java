3773f89b5197128e59fb5a4469434d06fddc4ea2
Evi Owhotemu <evicoach@gmail.com>
2023-09-30 23:41:26 +0100

    first commit

---
+++ src/main/java/dev/lydtech/dispatch/handler/OrderCreatedHandler.java
@@ -0,0 +1,23 @@
+package dev.lydtech.dispatch.handler;
+
+import dev.lydtech.dispatch.service.DispatchService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class OrderCreatedHandler {
+
+    private final DispatchService dispatchService;
+
+    public OrderCreatedHandler(DispatchService dispatchService) {
+        this.dispatchService = dispatchService;
+    }
+
+    @KafkaListener(id = "orderConsumerClient", topics = {"order.created"}, groupId = "dispatch.order.created.consumer")
+    public void listen(String payload) {
+        log.info("Received message payload: " + payload);
+        dispatchService.process(payload);
+    }
+}
